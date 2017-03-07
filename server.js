c7a4dfb11a688be1e92a6a9d012eda50c64681fd
Simon Ledbetter <simon.ledbetter@gmail.com>
2017-03-07 00:24:03 -0500

    testing access control header

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static(__dirname + '/public'));
 
 //set custom server-side headers
 app.all('*', function(req, res, next) {
-  // res.set('Access-Control-Allow-Origin', 'http://localhost');
+  res.set('Access-Control-Allow-Origin', 'http://localhost');
   // res.set('Access-Control-Allow-Credentials', true);
   // res.set('Access-Control-Allow-Methods', 'GET');
   // res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
