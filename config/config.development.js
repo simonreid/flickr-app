4e9282a11f7e0e95a5a5a528fd6a6a49db3fbcc2
Simon Ledbetter <simon.ledbetter@gmail.com>
2017-03-07 00:30:52 -0500

    various cleanup

--- config/config.development.js
+++ config/config.development.js
@@ -2,17 +2,17 @@ var config = require('./config.global');
 
 //development specific config goes here
 
-//mongodb
-config.dburl = 'mongodb://dbUser:dbUser@127.0.0.1:27017/test?authMechanism=SCRAM-SHA-1';
-config.env = 'development';
-//note to self: cannot set user and password in mongoOpts for whatever reason. default auth mechanism if not provided is MONGODB-CR.
-
-
-//jwt signature secret
-config.jwtsecret = 'weeeeee';
-config.jwtExpirationSeconds = 60 * 60 * 24; //1 day expiration
-
-config.sessionsecret = 'weeeeee';
+//api configuration
+config.api_config = {
+  api_key : 'a5e95177da353f58113fd60296e1d250',
+  user_id : '24662369@N07', // NASA
+  host : 'api.flickr.com',
+  path : '/services/rest',
+  query_params : {
+    nojsoncallback: 1,
+    format: 'json',
+  }
+};
 
 //export
 module.exports = config;
--- config/config.production.js
+++ config/config.production.js
@@ -3,3 +3,15 @@ var config = require('./config.global');
 //production specific config goes here
 config.env = 'production';
 module.exports = config;
+
+//api configuration
+config.api_config = {
+  api_key : 'a5e95177da353f58113fd60296e1d250',
+  user_id : '24662369@N07', // NASA
+  host : 'api.flickr.com',
+  path : '/services/rest',
+  query_params : {
+    nojsoncallback: 1,
+    format: 'json',
+  }
+};
