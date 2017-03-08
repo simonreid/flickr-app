432b5daf62f40d0b76dd81067fcf7dfae6f065a1
Simon Ledbetter <sledbetter@tivo.com>
2017-03-08 17:21:30 -0500

    testing ng-cloak and housekeeping

--- public/js/app.js
+++ public/js/app.js
@@ -8,8 +8,8 @@
  * Main module of the application.
  */
  var options = {};
-options.server_url = "http://flickr-app.mybluemix.net";
-//options.server_url = "http://localhost:3000";
+//options.server_url = "http://flickr-app.mybluemix.net";
+options.server_url = "http://localhost:3000";
 
  angular.module('flickr-app',
  [
